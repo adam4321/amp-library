7b00a1de066f3d8ad7339f0078075be739b11158
Adam Wright <adamjw321@gmail.com>
2018-12-12 15:55:25 -0800

    Connect Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
-import firebase from 'firebase';
+import firebase from './firebase.js';
 
 
 class App extends Component {
@@ -14,11 +13,26 @@ class App extends Component {
       username:''
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
 handleChange(e) {
   this.setState({
     [e.target.name]:e.target.value
+  });
+}
+
+handleSubmit(e) {
+  e.preventDefault();
+  const itemsRef = firebase.database().ref('items');
+  const item = {
+    title: this.state.currentItem,
+    user: this.state.username
+  }
+  itemsRef.push(item);
+  this.setState({
+    currentItem: '',
+    username: ''
   });
 }
 
@@ -33,7 +47,7 @@ handleChange(e) {
         </header>
         <div className='container'>
           <section className='add-item'>
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username}/>
                 <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem}/>
                 <button>Add Item</button>
