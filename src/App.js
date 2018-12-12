444e49c4440193f07f4bf53ac9bca6f085628787
Adam Wright <adamjw321@gmail.com>
2018-12-12 15:05:58 -0800

    connect input box

--- src/App.js
+++ src/App.js
@@ -13,8 +13,14 @@ class App extends Component {
       currentItem:'',
       username:''
     }
+    this.handleChange = this.handleChange.bind(this);
   }
   
+handleChange(e) {
+  this.setState({
+    [e.target.name]:e.target.value
+  });
+}
 
   render() {
     return (
@@ -28,7 +34,7 @@ class App extends Component {
         <div className='container'>
           <section className='add-item'>
               <form>
-                <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.user}/>
+                <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username}/>
                 <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem}/>
                 <button>Add Item</button>
               </form>
