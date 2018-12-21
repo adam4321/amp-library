e5770e4bdc9321455a61b5cc1bbceb4701d4981a
Adam Wright <adamjw321@gmail.com>
2018-12-21 00:29:23 -0800

    Improve desktop UI

--- src/App.js
+++ src/App.js
@@ -170,7 +170,7 @@ firebase
      <section className='add-item'>
        <h3 id='enterText'>Enter a New Amp</h3>
           <form onSubmit={this.handleSubmit}>
-            <input type="text" name="currentItem" placeholder="What is the Amp model?" onChange={this.handleChange} value={this.state.currentItem} />
+            <input className='ampNameField' type="text" name="currentItem" placeholder="What is the Amp model?" onChange={this.handleChange} value={this.state.currentItem} />
             <CustomUploadButton
                   handleImgUpload = {this.handleImgUpload}
                   accept="image/*"
@@ -180,10 +180,11 @@ firebase
                   onUploadSuccess={this.handleUploadSuccess}
                   onProgress={this.handleProgress}
                   style={{backgroundColor: 'steelblue', color: 'white', padding: 10, borderRadius: 4}}
+                  className={'ampImgButton'}
               >
             Add a Photo of the Amp
             </CustomUploadButton>
-            <input type="text" name="ampDescription" placeholder="Describe the Amplifier" onChange={this.handleChange} value={this.state.ampDescription}/>
+            <input className='descriptionField' type="text" name="ampDescription" placeholder="Describe the Amplifier" onChange={this.handleChange} value={this.state.ampDescription}/>
             <CustomUploadButton
                   handleSchematicUpload = {this.handleSchematicUpload}
                   accept="image/*"
@@ -193,10 +194,11 @@ firebase
                   onUploadSuccess={this.handleUploadSuccessSch}
                   onProgress={this.handleProgressSch}
                   style={{backgroundColor: 'steelblue', color: 'white', padding: 10, borderRadius: 4}}
+                  className={'schmaticButton'}
               >
             Add the Amp's Schematic
             </CustomUploadButton>
-            <button>Add a new Amplifier</button>
+            <button className='addButton'>Add a new Amplifier</button>
           </form>
       </section>
 
