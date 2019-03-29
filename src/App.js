8ac66ed634562e874fc05518152670bab9bb0217
Wright <wrighada@oregonstate.edu>
2019-03-28 19:13:00 -0700

    Improve style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-// ts-check
+// @ts-check
 
 import React, { Component } from 'react';
 import './App.css';
@@ -11,172 +11,173 @@ import Divider from '@material-ui/core/Divider';
 
 
 class App extends Component {
-  constructor() {
-    super();
+
+  constructor(props) {
+    super(props);
     this.state = {
-      currentItem:'',
-      username:'',
+      currentItem: '',
+      username: '',
       description:'',
       items: [],
       user: null,
       isUploading: false,
       progress: 0,
       ampImg: '',
-      schematic:'',
-      ampImgURL:'',
-      schematicURL:'',
+      schematic: '',
+      ampImgURL: '',
+      schematicURL: '',
       left: false
     }
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.login = this.login.bind(this);
-    this.logout= this.logout.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
-// toggle drawer function
+  // Toggle drawer function
 
-  toggleDrawer = (side, open) => () => {
+    toggleDrawer = (side, open) => () => {
+      this.setState({
+        [side]: open,
+      });
+    };
+    
+  // Logout function
+
+  logout() {
+    auth.signOut()
+      .then(() => {
+        this.setState({
+          user: null
+        });
+      });
+  }
+
+  // Login function
+
+  login() {
+    auth.signInWithPopup(provider) 
+      .then((result) => {
+        const user = result.user;
+        window.location.reload();
+        this.setState({
+          user
+        });
+      });
+  }
+    
+  // Function to handle UI changes
+
+  handleChange(e) {
     this.setState({
-      [side]: open,
+      [e.target.name]:e.target.value
     });
+  }
+
+  // Function to handle new amp submission
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.user.displayName || this.state.user.email,
+      description: this.state.ampDescription,
+      photo: this.state.ampImgURL,
+      layout: this.state.schematicURL
+    }
+    itemsRef.push(item);
+    this.setState({
+      currentItem: '',
+      username: '',
+      ampDescription: '',
+      photo:'',
+      layout:''
+    });
+  }
+
+  // Function For removing amps
+
+  removeItem(itemId) {
+    const itemRef = firebase.database().ref(`/items/${itemId}`);
+    itemRef.remove();
+  }
+
+  // Functions for uploading images to the database
+
+  handleImgUpload = event =>
+  this.setState({ username: event.target.value });
+  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+  handleProgress = progress => this.setState({ progress });
+  handleUploadError = error => {
+  this.setState({ isUploading: false });
+  console.error(error);
   };
-  
-// logout function
 
-logout() {
-  auth.signOut()
-    .then(() => {
+  // Function to change UI after image upload
+
+  handleUploadSuccess = filename => {
+  this.setState({ ampImg: filename, progress: 100, isUploading: false });
+  firebase
+    .storage()
+    .ref("images")
+    .child(filename)
+    .getDownloadURL()
+    .then(url => this.setState({ ampImgURL: url}));
+  };
+
+  // Function for uploading a schematic to database
+
+  handleSchematicUpload = event =>
+  this.setState({ username: event.target.value });
+  handleUploadStartSch = () => this.setState({ isUploading: true, progress: 0 });
+  handleProgressSch = progress => this.setState({ progress });
+  handleUploadErrorSch = error => {
+  this.setState({ isUploading: false });
+  console.error(error);
+  };
+
+  // Function for updating UI after schematic upload
+
+  handleUploadSuccessSch = filename => {
+  this.setState({ schematic: filename, progress: 100, isUploading: false });
+  firebase
+    .storage()
+    .ref("images")
+    .child(filename)
+    .getDownloadURL()
+    .then(url => this.setState({ schematicURL: url}));
+  };
+
+  // Function for return cards from the database after a user is logged in
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } 
+    });
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user,
+          description: items[item].description,
+          photo: items[item].photo,
+          layout: items[item].layout
+        });
+      }
       this.setState({
-        user: null
+        items: newState
       });
     });
-}
-
-// login function
-
-login() {
-  auth.signInWithPopup(provider) 
-    .then((result) => {
-      const user = result.user;
-      window.location.reload();
-      this.setState({
-        user
-      });
-    });
-}
-  
-// function to handle UI changes
-
-handleChange(e) {
-  this.setState({
-    [e.target.name]:e.target.value
-  });
-}
-
-// function to handle new amp submission
-
-handleSubmit(e) {
-  e.preventDefault();
-  const itemsRef = firebase.database().ref('items');
-
-  const item = {
-    title: this.state.currentItem,
-    user: this.state.user.displayName || this.state.user.email,
-    description: this.state.ampDescription,
-    photo: this.state.ampImgURL,
-    layout: this.state.schematicURL
   }
-  itemsRef.push(item);
-  this.setState({
-    currentItem: '',
-    username: '',
-    ampDescription: '',
-    photo:'',
-    layout:''
-  });
-}
-
-// function For removing amps
-
-removeItem(itemId) {
-  const itemRef = firebase.database().ref(`/items/${itemId}`);
-  itemRef.remove();
-}
-
-// functions for uploading images to the database
-
-handleImgUpload = event =>
-this.setState({ username: event.target.value });
-handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
-handleProgress = progress => this.setState({ progress });
-handleUploadError = error => {
-this.setState({ isUploading: false });
-console.error(error);
-};
-
-// function to change UI after image upload
-
-handleUploadSuccess = filename => {
-this.setState({ ampImg: filename, progress: 100, isUploading: false });
-firebase
-  .storage()
-  .ref("images")
-  .child(filename)
-  .getDownloadURL()
-  .then(url => this.setState({ ampImgURL: url}));
-};
-
-// function for uploading a schematic to database
-
-handleSchematicUpload = event =>
-this.setState({ username: event.target.value });
-handleUploadStartSch = () => this.setState({ isUploading: true, progress: 0 });
-handleProgressSch = progress => this.setState({ progress });
-handleUploadErrorSch = error => {
-this.setState({ isUploading: false });
-console.error(error);
-};
-
-// function for updating UI after schematic upload
-
-handleUploadSuccessSch = filename => {
-this.setState({ schematic: filename, progress: 100, isUploading: false });
-firebase
-  .storage()
-  .ref("images")
-  .child(filename)
-  .getDownloadURL()
-  .then(url => this.setState({ schematicURL: url}));
-};
-
-// function for return cards from the database after a user is logged in
-
-componentDidMount() {
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      this.setState({ user });
-    } 
-  });
-const itemsRef = firebase.database().ref('items');
-itemsRef.on('value', (snapshot) => {
-  let items = snapshot.val();
-  let newState = [];
-  for (let item in items) {
-    newState.push({
-      id: item,
-      title: items[item].title,
-      user: items[item].user,
-      description: items[item].description,
-      photo: items[item].photo,
-      layout: items[item].layout
-    });
-  }
-  this.setState({
-    items: newState
-  });
- });
-}
-
 
   render() {
   
@@ -187,7 +188,6 @@ itemsRef.on('value', (snapshot) => {
           </section>
       </div>
     );
-
 
     return (
       <div className='app'>
@@ -221,12 +221,12 @@ itemsRef.on('value', (snapshot) => {
          <h3 id='userName'>{this.state.user.displayName || this.state.user.email} </h3>
         </div>
 
-      {/* new amp input box */}
+      {/* New amp input box */}
 
       <div className='container'>
      <section className='add-item'>
 
-      {/* desktop menu */}
+      {/* Desktop menu */}
 
       <div className='ampAddBox'>
          <h3 id='enterText'>Enter a New Amp</h3>
@@ -262,7 +262,7 @@ itemsRef.on('value', (snapshot) => {
            </div>
            </section>
 
-      {/* mobile menu */}
+      {/* Mobile menu */}
 
          <section>
           <div>          
@@ -321,7 +321,7 @@ itemsRef.on('value', (snapshot) => {
       </div>
       </section>
 
-      {/* display the cards of amps */}
+      {/* Display the cards of amps */}
 
       <section className='display-item'>
        <div className="wrapper">
@@ -347,7 +347,7 @@ itemsRef.on('value', (snapshot) => {
       </div>
      </section>
     </div>
-  </div>
+   </div>
       :
     <div className='wrapper'>
       <p id='logComment'>You must be logged in to see the Amp Library and to submit to it.</p>
@@ -356,8 +356,8 @@ itemsRef.on('value', (snapshot) => {
     </div>
    }
     </div>
-  );
- }
+   );
+  }
 }
 
 export default App;
