f1b79c7d29a8ece2f9bab6ff547be8fa11d8b7a7
Adam Wright <adam@adamjwright.com>
2021-01-31 01:17:24 -0800

    switch to functional

--- src/AmpCards.js
+++ src/AmpCards.js
@@ -10,57 +10,51 @@ import './media-query.css';
 import ModalImage from 'react-modal-image'
 
 
-class AmpCards extends React.Component {
+function AmpCards(props) {
+    return (
+        <div className = "wrapper">
+            <ul>
 
-    render() {
-        return (
-            <div className = "wrapper">
-                <ul>
-                    {this.props.items.map(item => {
-                        return (
-                            <li key = {item.id}>
-                                <h3>{item.title}</h3>
-                                
-                                {/* 
-                                // @ts-ignore */}
-                                <ModalImage
-                                    className = "photo"
-                                    alt = "Guitar amplifier"
-                                    small = {item.photo}
-                                    large = {item.photo}
-                                />
+                {props.items.map(item => {
+                    return (
+                        <li key = {item.id}>
+                            <h3>{item.title}</h3>
+                            
+                            <ModalImage
+                                className = "photo"
+                                alt = "Guitar amplifier"
+                                small = {item.photo}
+                                large = {item.photo}
+                            />
 
-                                <p>{item.description}</p>
+                            <p>{item.description}</p>
 
-                                {/* 
-                                // @ts-ignore */}
-                                <ModalImage
-                                    className = "schematic"
-                                    alt = "Amp schematic"
-                                    small = {item.layout}
-                                    large = {item.layout}
-                                />
-                                
-                                <p id = "ampContributor">
-                                    Contributed by{' '}
-                                    {item.user}
-                                    {item.user === this.props.user.displayName ||
-                                        item.user === this.props.user.email ? (
-                                        <button
-                                            id = "removeButton"
-                                            onClick = {() => this.props.removeItem(item.id)}
-                                        >
-                                            Remove Amplifier
-                                        </button>
-                                    ) : null}
-                                </p>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        )
-    }
+                            <ModalImage
+                                className = "schematic"
+                                alt = "Amp schematic"
+                                small = {item.layout}
+                                large = {item.layout}
+                            />
+                            
+                            <p id = "ampContributor">
+                                Contributed by{' '}
+                                {item.user}
+                                {item.user === props.user.displayName || item.user === props.user.email ? (
+                                    <button
+                                        id = "removeButton"
+                                        onClick = {() => props.removeItem(item.id)}
+                                    >
+                                        Remove Amplifier
+                                    </button>
+                                ) : null}
+                            </p>
+                        </li>
+                    );
+                })}
+
+            </ul>
+        </div>
+    );
 }
 
 export default AmpCards;
