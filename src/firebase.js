f23336eac1ed4629e55ad6f88ac6b53a5186f206
Adam Wright <adam@adamjwright.com>
2021-02-11 01:35:04 -0800

    update credentials

--- src/firebase.js
+++ src/firebase.js
@@ -6,19 +6,19 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database'; 
 import 'firebase/storage';
-import firebase_credential from './credentials.js'; 
+import firebase_credentials from './credentials.js'; 
 
 const config = {
-    apiKey: firebase_credential,
+    apiKey: firebase_credentials.api_key,
     authDomain: "amp-library.firebaseapp.com",
     databaseURL: "https://amp-library.firebaseio.com",
     projectId: "amp-library",
     storageBucket: "amp-library.appspot.com",
-    messagingSenderId: "119108100352"
-  };
+    messagingSenderId: firebase_credentials.mess_id
+};
 
-  firebase.initializeApp(config);
-  
-  export const provider = new firebase.auth.GoogleAuthProvider();
-  export const auth = firebase.auth();
-  export default firebase;
+firebase.initializeApp(config);
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+export default firebase;
